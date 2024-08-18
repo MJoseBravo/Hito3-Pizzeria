0d98435fd544ea1a0d100d4ce6927a6de26155dd
María José Bravo <majosebra@gmail.com>
2024-08-18 16:37:42 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+import Home from "./components/Home"
+// import FormularioRegistro from "./components/Register"
+// import FormularioLogin from "./components/Login"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import './App.css'
+import Header from "./components/Header"
+import Cart from "./components/Cart"
+import pizzas from "./components/pizzas"
+
+function App () {
+  const [allPizzas, setAllPizzas] = useState(pizzas)
+  const [total, setTotal] = useState(0)
+  const [countPizzas, setCountPizzas] = useState(1)
+  
+  return (
+    <div>
+      <Navbar />
+      {/* <Home /> */}
+      {/* <FormularioRegistro/> */}
+       {/* <FormularioLogin />  */}
+      <Cart 
+        allPizzas={allPizzas}
+        setAllPizzas={setAllPizzas}
+        />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
